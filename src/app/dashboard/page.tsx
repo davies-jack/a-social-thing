448e81cf6486d5dc608b3aed51bf51f9114813f8
davies-jack <jackethdavies@hotmail.com>
2024-11-29 20:32:23 +0000

    Prefetching likes before mapping

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -15,6 +15,12 @@ export default async function DashboardPage() {
   }
 
   const timeline = await getTimeline(userId);
+  const prefetchedPosts = await Promise.all(
+    timeline.map(async (post) => ({
+      ...post,
+      hasLiked: await isLiked(post.id, userId),
+    }))
+  )
 
   const postStatus = async (formData: FormData) => {
     "use server";
@@ -35,8 +41,7 @@ export default async function DashboardPage() {
           <h2 className="text-headline-text text-lg font-bold">your timeline</h2>
           <ul className="mt-4 pr-4">
             {timeline.length === 0 && <li>no posts yet</li>}
-            {timeline.map(async (post: SinglePost) => {
-              const hasLiked = await isLiked(post.id, userId);
+            {prefetchedPosts.map((post: SinglePost) => {
               const toggleLikePost = async () => {
                 "use server";
                 await likePost(post.id, userId);
@@ -47,7 +52,7 @@ export default async function DashboardPage() {
                 <TimelinePost
                   key={post.id}
                   post={post}
-                  hasLiked={hasLiked}
+                  hasLiked={post.hasLiked}
                   toggleLikePost={toggleLikePost}
                 />
               );
