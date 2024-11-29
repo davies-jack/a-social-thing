024034b1db7b337896f8e4460f8844addb62e5f3
davies-jack <jackethdavies@hotmail.com>
2024-11-29 20:08:03 +0000

    Add colour scheme values to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,21 @@ export default {
   ],
   theme: {
     extend: {
+      gridTemplateColumns: {
+        'dashboard': '.4fr 1fr',
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+
+        'bg-primary': 'var(--bg-primary)',
+        'bg-secondary': 'var(--bg-secondary)',
+        'bg-card': 'var(--bg-card)',
+        'headline-text': 'var(--headline-text)',
+        'paragraph-text': 'var(--paragraph-text)',
+        'bg-button': 'var(--bg-button)',
+        'button-text': 'var(--button-text)',
+        'tertiary': 'var(--tertiary)', 
       },
     },
   },
