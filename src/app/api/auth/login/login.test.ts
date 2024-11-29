2cc9494ae50c4548222fc43375435546400d4e8c
davies-jack <jackethdavies@hotmail.com>
2024-11-29 17:18:18 +0000

    Login route testing for user null & errors

--- src/app/api/auth/login/login.test.ts
+++ src/app/api/auth/login/login.test.ts
@@ -28,6 +28,10 @@ async function createPostRequest(
 }
 
 const EMPTY_BODY = {};
+const NORMAL_BODY = {
+    email: 'test@test.com',
+    password: 'password',
+};
 const HEADERS = {
     'Content-Type': 'application/json',
 };
@@ -49,5 +53,35 @@ describe('POST /api/auth/login', () => {
 
         expect(response.status).toBe(400);
         expect(body.message).toBe('Email and password are required');
+    });
+    it('should return 404 if user not found', async () => {
+        (mockPrisma.user.findUnique as jest.Mock).mockResolvedValueOnce(null);
+
+        const req = await createPostRequest(
+            'http://localhost:3000/api/auth/login',
+            NORMAL_BODY,
+            HEADERS
+        );
+
+        const response = await POST(req);
+        const body = await response?.json();
+
+        expect(response?.status).toBe(404);
+        expect(body?.message).toBe('User not found');
+    });
+    it('should return 500 if error occurs', async () => {
+        (mockPrisma.user.findUnique as jest.Mock).mockRejectedValueOnce(new Error('Database error'));
+
+        const req = await createPostRequest(
+            'http://localhost:3000/api/auth/login',
+            NORMAL_BODY,
+            HEADERS
+        );
+
+        const response = await POST(req);
+        const body = await response?.json();
+
+        expect(response?.status).toBe(500);
+        expect(body?.message).toBe('Internal server error');
     });
 });
--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,3 +1,4 @@
+import prisma from '@/utils/db';
 import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
@@ -6,5 +7,14 @@ export async function POST(request: Request) {
     
     if (!email || !password) {
         return NextResponse.json({ message: 'Email and password are required' }, { status: 400 });
+    }
+
+    try {
+        const user = await prisma.user.findUnique({ where: { email } });
+        if (!user) {
+            return NextResponse.json({ message: 'User not found' }, { status: 404 });
+        }
+    } catch (error) {
+        return NextResponse.json({ message: 'Internal server error' }, { status: 500 });
     }
 }
