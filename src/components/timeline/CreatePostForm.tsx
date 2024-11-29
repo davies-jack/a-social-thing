7b6f4d63b43a1bd6fd007e84d5572a0f83db6122
davies-jack <jackethdavies@hotmail.com>
2024-11-29 20:43:38 +0000

    Enforcing 175 character limit

--- src/components/timeline/CreatePostForm.tsx
+++ src/components/timeline/CreatePostForm.tsx
@@ -1,11 +1,23 @@
+"use client";
+
+import { useState } from "react";
 type Props = {
-  onSubmit: (formData: FormData) => Promise<void>;
+  onSubmit: (
+    formData: FormData,
+  ) => Promise<void>;
 };
 
 export default function CreatePostForm({ onSubmit }: Props) {
+  const [post, setPost] = useState("");
+
   return (
     <form
-      action={onSubmit}
+      action={(formData) => {
+        if (post.length < 175) {
+          onSubmit(formData);
+          setPost("");
+        }
+      }}
       className="w-full flex flex-col gap-2 items-start p-6"
     >
       <textarea
@@ -13,13 +25,28 @@ export default function CreatePostForm({ onSubmit }: Props) {
         id="status"
         placeholder="what's on your mind?"
         className="w-full resize-none rounded-md p-2 text-black"
+        onChange={(e) => {
+          setPost(e.target.value);
+        }}
+        value={post}
       />
-      <button
-        type="submit"
-        className="rounded-md p-2 px-4 text-sm text-button-text bg-bg-button font-bold self-end"
-      >
-        post your status
-      </button>
+      <div className="w-full flex flex-row justify-between items-center gap-2">
+        <button
+          type="submit"
+          className="rounded-md p-2 px-4 text-sm text-button-text bg-bg-button font-bold self-end"
+        >
+          post your status
+        </button>
+        <p
+          className={`
+            text-sm text-button-text
+            font-bold
+            ${post.length > 175 ? "text-red-500" : ""}
+          `}
+        >
+          {post.length} / 175
+        </p>
+      </div>
     </form>
   );
 }
