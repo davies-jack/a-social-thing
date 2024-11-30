03f7cd1590e4cebc283838c303cdbfa7610e9f93
davies-jack <jackethdavies@hotmail.com>
2024-11-30 01:53:01 +0000

    Adding error handling in CreatePostForm

--- src/components/timeline/CreatePostForm.tsx
+++ src/components/timeline/CreatePostForm.tsx
@@ -9,23 +9,41 @@ type Props = {
 
 export default function CreatePostForm({ onSubmit }: Props) {
   const [post, setPost] = useState("");
+  const [noContentError, setNoContentError] = useState(false);
 
   return (
     <form
       action={(formData) => {
-        if (post.length <= 175) {
+        setNoContentError(false);
+
+        if (post.length === 0) {
+          setNoContentError(true);
+        }
+
+        if (post.length <= 175 && post.length > 0) {
           onSubmit(formData);
           setPost("");
         }
       }}
       className="w-full flex flex-col gap-2 items-start p-6"
     >
+      {noContentError && (
+        <p className="text-red-500 text-sm m-0 p-0 font-bold">
+          please enter a post.
+        </p>
+      )}
+      {post.length > 175 && (
+        <p className="text-red-500 text-sm m-0 p-0 font-bold">
+          your post is too long. please keep it under 175 characters.
+        </p>
+      )}
       <textarea
         name="status"
         id="status"
         placeholder="what's on your mind?"
         className="w-full resize-none rounded-md p-2 text-black"
         onChange={(e) => {
+          setNoContentError(false);
           setPost(e.target.value);
         }}
         value={post}
