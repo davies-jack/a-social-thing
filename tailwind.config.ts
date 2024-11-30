39c34db74c3b627b74e32b6ce8a7e38cb57041af
davies-jack <jackethdavies@hotmail.com>
2024-11-30 19:34:00 +0000

    Adding more tailwind config colours

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,11 +16,13 @@ export default {
         foreground: "var(--foreground)",
 
         'bg-primary': 'var(--bg-primary)',
+        'primary': 'var(--bg-primary)',
         'bg-secondary': 'var(--bg-secondary)',
         'bg-card': 'var(--bg-card)',
         'headline-text': 'var(--headline-text)',
         'paragraph-text': 'var(--paragraph-text)',
         'bg-button': 'var(--bg-button)',
+        'bg-button-secondary': 'var(--bg-button-secondary)',
         'button-text': 'var(--button-text)',
         'tertiary': 'var(--tertiary)', 
       },
