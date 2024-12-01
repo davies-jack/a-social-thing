44709ff43bd5e8ea1e9ec78cc6cea75607cb4470
davies-jack <jackethdavies@hotmail.com>
2024-12-01 19:55:01 +0000

    Adding className functionality to Pill

--- src/components/Pill.tsx
+++ src/components/Pill.tsx
@@ -2,15 +2,18 @@ import React from 'react'
 
 type Props = {
     children: React.ReactNode;
+    className?: string;
 }
 
-export default function Pill({ children }: Props) {
+export default function Pill({ children, className }: Props) {
   return (
-    <div className="hover:text-headline-text cursor-pointer
+    <div className={`hover:text-headline-text cursor-pointer
         rounded-md bg-bg-card border-2 border-bg-primary px-2 py-1
         transition-colors duration-75
         hover:bg-bg-primary hover:border-2 hover:border-transparent hover:shadow-md
-        text-xs">
+        text-xs
+        ${className}
+        `}>
         {children}
     </div>
   )
