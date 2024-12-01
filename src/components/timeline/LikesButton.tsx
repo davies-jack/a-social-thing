654d68f51979622d445885298deb189fe54561a9
davies-jack <jackethdavies@hotmail.com>
2024-12-01 23:48:39 +0000

    Adding heart svg to LikesButton

--- src/components/timeline/LikesButton.tsx
+++ src/components/timeline/LikesButton.tsx
@@ -6,7 +6,7 @@ type Props = {
   likeAmount: number;
   hasLiked: boolean;
   toggleLikePost: () => void;
-  className?: string; 
+  className?: string;
 };
 
 export default function LikesButton({
@@ -23,13 +23,24 @@ export default function LikesButton({
         transition-colors duration-75
         hover:bg-bg-primary hover:border-2 hover:border-transparent hover:shadow-md
         text-xs
-        ${hasLiked && "border-2 border-red-600 hover:border-red-800"}
+        flex flex-row gap-2 items-center
         ${className}
       `}
       onClick={toggleLikePost}
     >
-      {hasLiked ? "unlike" : "like"}{" "}
-      <span className="text-headline-text font-bold">{likeAmount}</span>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="10"
+        height="10"
+        viewBox="0 0 24 24"
+        fill={hasLiked ? "red" : "white"}
+      >
+        <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
+      </svg>
+      <div>
+        {hasLiked ? "unlike" : "like"}{" "}
+        <span className="text-headline-text font-bold">{likeAmount}</span>
+      </div>
     </div>
   );
 }
