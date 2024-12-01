113c4592ea488d93f2f80dea3dd3ac2b8efc3b0d
davies-jack <jackethdavies@hotmail.com>
2024-12-01 18:32:30 +0000

    Allowing custom classes within LikesButton

--- src/components/timeline/LikesButton.tsx
+++ src/components/timeline/LikesButton.tsx
@@ -6,20 +6,29 @@ type Props = {
   likeAmount: number;
   hasLiked: boolean;
   toggleLikePost: () => void;
+  className?: string; 
 };
 
 export default function LikesButton({
   likeAmount,
   hasLiked,
   toggleLikePost,
+  className,
 }: Props) {
   return (
-    <li
-      className="hover:text-headline-text cursor-pointer"
+    <div
+      className={`
+        hover:text-headline-text cursor-pointer
+        rounded-md bg-bg-card border-2 border-bg-primary px-2 py-1
+        transition-colors duration-75
+        hover:bg-bg-primary hover:border-2 hover:border-transparent hover:shadow-md
+        text-xs
+        ${className}
+      `}
       onClick={toggleLikePost}
     >
       {hasLiked ? "unlike" : "like"}{" "}
       <span className="text-headline-text font-bold">{likeAmount}</span>
-    </li>
+    </div>
   );
 }
