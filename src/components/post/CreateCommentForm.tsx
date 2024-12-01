9ad5766cecf85df4043f45b0b2aabe18c7e362de
davies-jack <jackethdavies@hotmail.com>
2024-12-01 21:18:11 +0000

    Taking create comment method to CreateCommentForm

--- src/components/post/CreateCommentForm.tsx
+++ src/components/post/CreateCommentForm.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Button from "../Button";
 
-export default function CreateCommentForm() {
+type Props = {
+  handleCreateComment: (comment: string) => Promise<void>;
+};
+
+export default function CreateCommentForm({ handleCreateComment }: Props) {
+  const [comment, setComment] = useState("");
   return (
     <form role="form">
       <div className="mt-4 flex flex-col items-center justify-center w-1/2 mx-auto">
@@ -9,9 +16,9 @@ export default function CreateCommentForm() {
           <label htmlFor="comment" className="text-headline-text text-sm font-bold text-left">
             add a comment
           </label>
-          <textarea id="comment" className="block resize-none w-full h-24 p-2 rounded-md border border-border-secondary"></textarea>
+          <textarea id="comment" className="block resize-none w-full h-24 p-2 rounded-md border border-border-secondary" value={comment} onChange={(e) => setComment(e.target.value)}></textarea>
         </div>
-        <Button label="post" className="mt-4" />
+        <Button label="post" className="mt-4" onClick={() => handleCreateComment(comment)} />
       </div>
     </form>
   );
