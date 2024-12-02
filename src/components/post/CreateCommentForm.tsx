41fac1091895daffc030ad7a399c1a6e0892a988
davies-jack <jackethdavies@hotmail.com>
2024-12-02 00:10:51 +0000

    Format create comment form

--- src/components/post/CreateCommentForm.tsx
+++ src/components/post/CreateCommentForm.tsx
@@ -9,17 +9,37 @@ type Props = {
 
 export default function CreateCommentForm({ handleCreateComment }: Props) {
   const [comment, setComment] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
+    await handleCreateComment(comment);
+    setComment("");
+    setIsLoading(false);
+  }
   return (
-    <form role="form">
-      <div className="mt-4 flex flex-col items-center justify-center w-1/2 mx-auto">
-        <div>
-          <label htmlFor="comment" className="text-headline-text text-sm font-bold text-left">
-            add a comment
-          </label>
-          <textarea id="comment" className="block resize-none w-full h-24 p-2 rounded-md border border-border-secondary" value={comment} onChange={(e) => setComment(e.target.value)}></textarea>
-        </div>
-        <Button label="post" className="mt-4" onClick={() => handleCreateComment(comment)} />
+    <div className="mt-4">
+      <div>
+        <label
+          htmlFor="comment"
+          className="text-headline-text text-sm font-bold text-left"
+        >
+          add a comment
+        </label>
+        <textarea
+          id="comment"
+          className="block resize-none w-full p-2 rounded-md border border-border-secondary"
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+        ></textarea>
       </div>
-    </form>
+      <Button
+        label="post"
+        className="mt-4 self-end"
+        onClick={handleSubmit}
+        disabled={isLoading}
+      />
+    </div>
   );
 }
