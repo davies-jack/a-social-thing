065c1b5ab1c091954f278e8b992aa40bebd1d2c7
davies-jack <jackethdavies@hotmail.com>
2024-12-16 00:41:23 +0000

    Revamping error component

--- src/components/atoms/Error/index.tsx
+++ src/components/atoms/Error/index.tsx
@@ -1,19 +1,28 @@
 import React from 'react'
 
 type Props = {
-    message: string;
-    className?: string;
+    type: "error" | "warning" | "info" | "success";
+    children: React.ReactNode;
 }
 
-export default function Error({ message, className }: Props) {
+export default function Error({ children, type = 'info' }: Props) {
+  const bgColorLookup = {
+    error: 'bg-error-red',
+    warning: 'bg-warning-amber',
+    info: 'bg-info-blue',
+    success: 'bg-success-green',
+  }
   return (
     <div className={`
-        bg-red-700
-        text-white text-xs lowercase font-bold
-        rounded-md py-1 px-2
-        ${className}
+        ${bgColorLookup[type]}
+        text-black text-base lowercase
+        rounded-md px-5 py-3
+        flex flex-col gap-1
+        shadow-md
     `}>
-        {message}!
+        <span className="text-xs text-black">
+          {children}
+        </span>
     </div>
   )
 }
