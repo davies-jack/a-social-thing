b801262cb9a66edd9d621ec06ae654cb5549deb0
davies-jack <jackethdavies@hotmail.com>
2024-12-17 03:11:17 +0000

    Deleting backup old components

--- src/h/container/content/ContainerContent.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {
-  children: React.ReactNode;
-};
-
-export default function ContainerContent({ children }: Props) {
-  return <div className="flex flex-col gap-2 text-sm text-paragraph-text">{children}</div>;
-}
