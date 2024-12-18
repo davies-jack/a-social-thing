fe4d3bb20885377ed8b662decc46bc4ee7c793e0
davies-jack <jackethdavies@hotmail.com>
2024-12-18 01:29:21 +0000

    Changing view profile

--- src/app/(authenticated)/profile/[username]/page.tsx
+++ src/app/(authenticated)/profile/[username]/page.tsx
@@ -1,4 +1,7 @@
+import Container from "@/components/atoms/Container";
+import Paragraph from "@/components/atoms/Paragraph";
 import Pill from "@/components/atoms/Pill/";
+import TimelinePost from "@/components/molecules/TimelinePost";
 import { formatDate } from "@/utils/date";
 import prisma from "@/utils/db";
 import { getPosts } from "@/utils/posts";
@@ -65,66 +68,47 @@ export default async function ProfilePage({ params }: Props) {
   const posts = await getPosts(profileUserId);
 
   return (
-    <main>
-      <div className="bg-bg-secondary p-6 rounded-md">
-        <h1 className="text-headline-text text-lg font-bold">{username}</h1>
-        <div className="mt-1 flex flex-row gap-2 text-sm">
-          <span className="hover:text-headline-text hover:cursor-pointer">
-            {amountOfFollowers} followers
-          </span>
-          <span className="hover:text-headline-text hover:cursor-pointer">
-            {amountOfFollowing} following
-          </span>
+    <section>
+      <Container>
+        <h1 className="flex flex-row items-center justify-between gap-2">
+          <div className="flex flex-row items-center gap-2">
+            <div className="animate-pulse h-2 w-2 rounded-full bg-[lime]"></div> {username}
+          </div>
+          <div className="flex flex-row items-center gap-1">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="14"
+              height="14"
+              viewBox="0 0 24 24"
+              fill="#94a1b2"
+            >
+              <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z" />
+            </svg>
+            <Paragraph>manchester</Paragraph>
+          </div>
+        </h1>
+        <ul className="flex flex-row gap-2 mt-2">
+          <li>
+            <Pill darker>{amountOfFollowers} followers</Pill>
+          </li>
+          <li>
+            <Pill darker>{amountOfFollowing} following</Pill>
+          </li>
+        </ul>
+        <div className="mt-4">
+          <Paragraph>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam dolorum quasi et
+            quaerat molestias obcaecati, explicabo necessitatibus mollitia alias est laboriosam rerum.
+            Iusto nesciunt, dolorum eveniet itaque quidem hic cumque?
+          </Paragraph>
         </div>
+      </Container>
 
-        {!isOurProfile && (
-          <form action={handleFollowUser}>
-            <button
-              className={`text-button-text p-2 px-4 rounded-md text-sm mt-4 font-bold
-              ${
-                following
-                  ? "bg-bg-button-secondary border border-bg-button hover:bg-bg-button transition-colors duration-75"
-                  : "bg-bg-button hover:bg-bg-button-secondary shadow-md border"
-              }
-              `}
-            >
-              {following ? "unfollow" : "follow"}
-            </button>
-          </form>
-        )}
-      </div>
-      <ul className="rounded-md m-6 text-sm text-headline-text py-1">
-        {posts.map((post, index) => {
-          const isFirst = index === 0;
-          let likesLabel: React.ReactNode;
-          if (post.likes.length === 0 || post.likes.length > 1) {
-            likesLabel = (
-              <span className="text-paragraph-text">
-                <strong className="text-headline-text">{post.likes.length}</strong> likes
-              </span>
-            );
-          } else {
-            likesLabel = (
-              <span className="text-paragraph-text">
-                <strong className="text-headline-text">{post.likes.length}</strong> like
-              </span>
-            );
-          }
-
-          return (
-            <li
-              key={post.id}
-              className={`bg-bg-secondary p-6 ${isFirst ? "mt-0" : "mt-4"} rounded-md`}
-            >
-              <span>{post.status}</span>
-              <span className="block font-bold text-paragraph-text text-xs mt-2">
-                {formatDate(post.createdAt)}
-              </span>
-              <Pill className="w-fit mt-2 cursor-default">{likesLabel}</Pill>
-            </li>
-          );
+      <ul className="mt-4 flex flex-col gap-4 w-2/3 mx-auto">
+        {posts.map((post) => {
+          return <TimelinePost key={post.id} post={post} />;
         })}
       </ul>
-    </main>
+    </section>
   );
 }
